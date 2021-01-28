5309d3dcd838289a95fe38cedf4235938fd35bad
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-01-27 23:20:20 -0300

    CmbProjectView: add simple TreeView for CmbProject

---
+++ cambalache/cmb_project_view.py
@@ -0,0 +1,36 @@
+#
+# CmbProjectView - Cambalache Project View
+#
+# Copyright (C) 2021  Juan Pablo Ugarte - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+#
+
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, Gtk
+
+from .cmb_object import CmbUI, CmbObject
+
+
+class CmbProjectView(Gtk.TreeView):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.set_headers_visible (False)
+
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn('Object(Type)', renderer)
+        column.set_cell_data_func(renderer, self._name_cell_data_func, None)
+        self.append_column(column)
+
+    def _name_cell_data_func(self, column, cell, model, iter_, data):
+        obj = model.get_value(iter_, 0)
+
+        if type(obj) == CmbObject:
+            cell.set_property('text', f'{obj.name}({obj.type_id})')
+        elif type(obj) == CmbUI:
+            cell.set_property('text', obj.filename)
+
