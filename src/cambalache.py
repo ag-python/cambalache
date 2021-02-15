5c932c919871d5a8474d4638ac268d3d61ac3bba
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-02-14 23:32:17 -0300

    GUI: move most actions to CmbWindow
    CmbWindow: add create new project screen

--- src/cambalache.py
+++ src/cambalache.py
@@ -11,7 +11,7 @@ import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import GObject, Gtk, Gio
+from gi.repository import Gtk, Gio
 
 from cambalache import *
 
@@ -23,17 +23,11 @@ class CmbApplication(Gtk.Application):
         super().__init__(application_id='org.gnome.jpu.Cambalache',
                          flags=Gio.ApplicationFlags.FLAGS_NONE)
         self.window = None
-        self.project = None
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
-        for action in ['open', 'new',
-                       'undo', 'redo',
-                       'save', 'save_as',
-                       'add_ui', 'remove_ui',
-                       'import', 'export',
-                       'close', 'about', 'quit']:
+        for action in ['quit']:
             gaction= Gio.SimpleAction.new(action, None)
             gaction.connect("activate", getattr(self, f'_on_{action}_activate'))
             self.add_action(gaction)
@@ -44,85 +38,12 @@ class CmbApplication(Gtk.Application):
 
         self.window.present()
 
-    def _file_open_dialog_new(self, title, action=Gtk.FileChooserAction.OPEN, filter_obj=None):
-        dialog = Gtk.FileChooserDialog(
-            title=title,
-            parent=self.window,
-            action=action,
-            filter=filter_obj
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-
-        return dialog
-
-    def _on_open_activate(self, action, data):
-        dialog = self._file_open_dialog_new("Choose file to open",
-                                            filter_obj=self.window.open_filter)
-        if dialog.run() == Gtk.ResponseType.OK:
-            self.window.project = CmbProject(filename=dialog.get_filename())
-
-        dialog.destroy()
-
-    def _on_new_activate(self, action, data):
-        if self.window.project is None:
-            self.window.project = CmbProject()
-
-        self.window.stack.set_visible_child_name('workspace')
-
-    def _on_undo_activate(self, action, data):
-        print('_on_undo_activate')
-
-    def _on_redo_activate(self, action, data):
-        print('_on_redo_activate')
-
-    def _on_save_activate(self, action, data):
-        print('_on_save_activate')
-
-    def _on_save_as_activate(self, action, data):
-        print('_on_save_as_activate')
-
-    def _on_add_ui_activate(self, action, data):
-        dialog = self._file_open_dialog_new("Choose a file to save the project",
-                                            Gtk.FileChooserAction.SAVE)
-        if dialog.run() == Gtk.ResponseType.OK:
-            self.window.project.add_ui(dialog.get_filename())
-
-        dialog.destroy()
-
-    def _on_remove_ui_activate(self, action, data):
-        print('_on_remove_ui_activate')
-
-    def _on_import_activate(self, action, data):
-        if self.window.project is None:
-            return
-
-        dialog = self._file_open_dialog_new("Choose file to import",
-                                            filter_obj=self.window.import_filter)
-        if dialog.run() == Gtk.ResponseType.OK:
-            self.window.project.import_file(dialog.get_filename())
-
-        dialog.destroy()
-
-    def _on_export_activate(self, action, data):
-        if self.window.project is not None:
-            self.window.project.export()
-
-    def _on_close_activate(self, action, data):
-        self.window.project = None
-        self.window.stack.set_visible_child_name('cambalache')
-
-    def _on_about_activate(self, action, data):
-        self.window.about_dialog.present()
-
     def _on_quit_activate(self, action, data):
         self.quit()
 
 
 if __name__ == "__main__":
+    # FIXME: we need this to load template resources
+    os.chdir(os.path.dirname(__file__))
     app = CmbApplication()
     app.run(sys.argv)
--- src/cmb_window.py
+++ src/cmb_window.py
@@ -11,7 +11,7 @@ import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import GObject, Gtk
+from gi.repository import GObject, Gio, Gtk
 
 from cambalache import *
 
@@ -27,6 +27,17 @@ class CmbWindow(Gtk.ApplicationWindow):
     import_button_box = Gtk.Template.Child()
 
     stack = Gtk.Template.Child()
+
+    # Start screen
+    version_label = Gtk.Template.Child()
+
+    # New Project
+    np_name_entry = Gtk.Template.Child()
+    np_location_chooser = Gtk.Template.Child()
+    np_gtk3_radiobutton = Gtk.Template.Child()
+    np_gtk4_radiobutton = Gtk.Template.Child()
+
+    # Workspace
     view = Gtk.Template.Child()
     tree_view = Gtk.Template.Child()
     type_entry = Gtk.Template.Child()
@@ -44,6 +55,16 @@ class CmbWindow(Gtk.ApplicationWindow):
 
         self.connect("notify::project", self._on_project_notify)
 
+        for action in ['open', 'create_new', 'new',
+                       'undo', 'redo',
+                       'save', 'save_as',
+                       'add_ui', 'remove_ui',
+                       'import', 'export',
+                       'close', 'about']:
+            gaction= Gio.SimpleAction.new(action, None)
+            gaction.connect("activate", getattr(self, f'_on_{action}_activate'))
+            self.add_action(gaction)
+
     def _on_project_notify(self, obj, pspec):
         self.view.project = self.project
         self.tree_view.props.model = self.project
@@ -57,4 +78,88 @@ class CmbWindow(Gtk.ApplicationWindow):
     @Gtk.Template.Callback('on_type_entry_activate')
     def _on_type_entry_activate(self, entry):
         self.project.add_object(1, entry.get_text())
+
+    def _file_open_dialog_new(self, title, action=Gtk.FileChooserAction.OPEN, filter_obj=None):
+        dialog = Gtk.FileChooserDialog(
+            title=title,
+            parent=self,
+            action=action,
+            filter=filter_obj
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK,
+        )
+
+        return dialog
+
+    def _on_open_activate(self, action, data):
+        dialog = self._file_open_dialog_new("Choose file to open",
+                                            filter_obj=self.open_filter)
+        if dialog.run() == Gtk.ResponseType.OK:
+            self.project = CmbProject(filename=dialog.get_filename())
+            self.stack.set_visible_child_name('workspace')
+        dialog.destroy()
+
+    def _on_create_new_activate(self, action, data):
+        self.stack.set_visible_child_name('new_project')
+
+    def _on_new_activate(self, action, data):
+        if self.project is not None:
+            return
+
+        name = self.np_name_entry.props.text
+        location = self.np_location_chooser.get_filename() or '.'
+
+        filename = os.path.join(location, name + '.cmb')
+        self.project = CmbProject(filename=filename)
+        self.stack.set_visible_child_name('workspace')
+
+    def _on_undo_activate(self, action, data):
+        print('_on_undo_activate')
+
+    def _on_redo_activate(self, action, data):
+        print('_on_redo_activate')
+
+    def _on_save_activate(self, action, data):
+        if self.project is not None:
+            self.project.save()
+
+    def _on_save_as_activate(self, action, data):
+        print('_on_save_as_activate')
+
+    def _on_add_ui_activate(self, action, data):
+        dialog = self._file_open_dialog_new("Choose a file to save the project",
+                                            Gtk.FileChooserAction.SAVE)
+        if dialog.run() == Gtk.ResponseType.OK:
+            self.project.add_ui(dialog.get_filename())
+
+        dialog.destroy()
+
+    def _on_remove_ui_activate(self, action, data):
+        print('_on_remove_ui_activate')
+
+    def _on_import_activate(self, action, data):
+        if self.project is None:
+            return
+
+        dialog = self._file_open_dialog_new("Choose file to import",
+                                            filter_obj=self.import_filter)
+        if dialog.run() == Gtk.ResponseType.OK:
+            self.project.import_file(dialog.get_filename())
+
+        dialog.destroy()
+
+    def _on_export_activate(self, action, data):
+        if self.project is not None:
+            self.project.export()
+
+    def _on_close_activate(self, action, data):
+        self.project = None
+        self.stack.set_visible_child_name('cambalache')
+
+    def _on_about_activate(self, action, data):
+        self.about_dialog.present()
 
