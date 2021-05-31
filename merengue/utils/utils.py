41a85c4213e867bd594900b52eaf6868229865ab
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-05-31 18:50:32 -0300

    Utils.object_get_builder_id: use g_object_get_data('gtk-builder-name')
    Use C function to get data for non buildable objects

--- merengue/utils/utils.py
+++ merengue/utils/utils.py
@@ -38,10 +38,13 @@ def object_get_builder_id(obj):
     if obj is None:
         return None
 
-    if Gtk.MAJOR_VERSION == 3:
-        return Gtk.Buildable.get_name(obj)
+    if issubclass(type(obj), Gtk.Buildable):
+        if Gtk.MAJOR_VERSION == 3:
+            return Gtk.Buildable.get_name(obj)
+        else:
+            return Gtk.Buildable.get_buildable_id (obj)
     else:
-        return Gtk.Buildable.get_buildable_id (obj)
+        return _g_object_get_data(obj, 'gtk-builder-name')
 
 
 def object_get_id(obj):
@@ -63,3 +66,28 @@ def object_get_name(obj):
 
     return None
 
+
+#
+# CTYPES HACKS
+#
+import ctypes
+from ctypes.util import find_library
+
+# Python object size, needed to calculate GObject pointer
+object_size = sys.getsizeof(object())
+
+# Return C object pointer
+def _cobject(obj):
+    return ctypes.c_void_p.from_address(id(obj) + object_size)
+
+
+# GObject library
+gobject = ctypes.CDLL(find_library('gobject-2.0'))
+
+# g_object_get_data
+gobject.g_object_get_data.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+gobject.g_object_get_data.restype = ctypes.c_char_p
+
+def _g_object_get_data (obj, key):
+    retval = gobject.g_object_get_data(_cobject(obj), key.encode('utf-8'))
+    return retval.decode('utf-8') if retval else None
