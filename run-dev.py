4479688d575ed2449b00cc709dddf065edb4db79
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-06-16 10:31:32 -0300

    run-dev.py: improve dev script

--- run-dev.py
+++ run-dev.py
@@ -20,6 +20,8 @@ sys.path.insert(1, basedir)
 
 os.environ['PATH'] = os.path.join(basedir, 'merengue') + ':' + os.environ.get('PATH')
 
+glib_compile_resources = GLib.find_program_in_path ('glib-compile-resources')
+
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def dev_config(filename, content):
@@ -44,16 +46,18 @@ def get_resource_mtime(filename):
     return max_mtime
 
 def compile_resource(dirname, filename):
+    if glib_compile_resources is None:
+        return
+
     filename_xml = f'{filename}.xml'
     resource = os.path.join(basedir, dirname, filename)
     resource_xml = os.path.join(basedir, dirname, filename_xml)
 
     if not os.path.exists(resource) or \
        os.path.getmtime (resource) < get_resource_mtime(resource_xml):
-        compiler = GLib.find_program_in_path ('glib-compile-resources')
         print('glib-compile-resources', dirname, filename_xml)
         GLib.spawn_sync(dirname,
-                        [compiler, filename_xml],
+                        [glib_compile_resources, filename_xml],
                         None,
                         GLib.SpawnFlags.DEFAULT,
                         None,
@@ -69,28 +73,32 @@ def configure_file(input_file, output_file, config):
         with open(output_file, 'w') as outfd:
             outfd.write(content)
 
-
-# Create config files pointing to source directories
-dev_config('cambalacheui/config.py',
-           f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('cambalacheui')}'")
-dev_config('merengue/config.py',
-           f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('merengue')}'")
-dev_config('src/config.py',
-           f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('src')}'")
-
-configure_file('merengue/merengue.in', 'merengue/merengue', {
-    'PYTHON': GLib.find_program_in_path('python3'),
-    'pkgdatadir': os.path.abspath('.')
-})
-os.chmod('merengue/merengue', stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC)
-
-# Ensure gresources are up to date
-compile_resource('cambalacheui', 'cambalacheui.gresource')
-compile_resource('merengue', 'merengue.gresource')
-compile_resource('src', 'cambalache.gresource')
-
-from src import CmbApplication
-
 if __name__ == '__main__':
+    if glib_compile_resources is None:
+        print('Could not find glib-compile-resources in PATH')
+        exit()
+
+    # Create config files pointing to source directories
+    dev_config('cambalacheui/config.py',
+               f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('cambalacheui')}'")
+    dev_config('merengue/config.py',
+               f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('merengue')}'")
+    dev_config('src/config.py',
+               f"VERSION = 'git'\npkgdatadir = '{os.path.abspath('src')}'")
+
+    # Create merengue bin script
+    configure_file('merengue/merengue.in', 'merengue/merengue', {
+        'PYTHON': GLib.find_program_in_path('python3'),
+        'pkgdatadir': os.path.abspath('.')
+    })
+    os.chmod('merengue/merengue', stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC)
+
+    # Ensure gresources are up to date
+    compile_resource('cambalacheui', 'cambalacheui.gresource')
+    compile_resource('merengue', 'merengue.gresource')
+    compile_resource('src', 'cambalache.gresource')
+
+    # Run Application
+    from src import CmbApplication
     app = CmbApplication()
     app.run(sys.argv)
