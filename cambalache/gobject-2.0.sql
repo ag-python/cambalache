e7642039490e278f59502307d47c1c255c086feb
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-08-26 16:39:27 -0400

    Type Data: update gtk data
    Update data to fix enum/flags names
    
    GLib uses the c identifier as the enum/flag name not the name provided in Gir
    
    Closes issue #22

--- cambalache/gobject-2.0.sql
+++ cambalache/gobject-2.0.sql
@@ -8,10 +8,10 @@ INSERT INTO type VALUES
 ('GObject', 'object', 'gobject', NULL, NULL, NULL, NULL);
 
 INSERT INTO type_flags VALUES
-('GBindingFlags', 'bidirectional', 'bidirectional', 1, 'G_BINDING_BIDIRECTIONAL', 'Bidirectional binding; if either the property of the source or the property of the target changes, the other is updated.'),
-('GBindingFlags', 'default', 'default', 0, 'G_BINDING_DEFAULT', 'The default binding; if the source property changes, the target property is updated with its value.'),
-('GBindingFlags', 'invert_boolean', 'invert-boolean', 4, 'G_BINDING_INVERT_BOOLEAN', 'If the two properties being bound are booleans, setting one to %TRUE will result in the other being set to %FALSE and vice versa. This flag will only work for boolean properties, and cannot be used when passing custom transformation functions to g_object_bind_property_full().'),
-('GBindingFlags', 'sync_create', 'sync-create', 2, 'G_BINDING_SYNC_CREATE', 'Synchronize the values of the source and target properties when creating the binding; the direction of the synchronization is always from the source to the target.');
+('GBindingFlags', 'G_BINDING_BIDIRECTIONAL', 'bidirectional', 1, 'Bidirectional binding; if either the property of the source or the property of the target changes, the other is updated.'),
+('GBindingFlags', 'G_BINDING_DEFAULT', 'default', 0, 'The default binding; if the source property changes, the target property is updated with its value.'),
+('GBindingFlags', 'G_BINDING_INVERT_BOOLEAN', 'invert-boolean', 4, 'If the two properties being bound are booleans, setting one to %TRUE will result in the other being set to %FALSE and vice versa. This flag will only work for boolean properties, and cannot be used when passing custom transformation functions to g_object_bind_property_full().'),
+('GBindingFlags', 'G_BINDING_SYNC_CREATE', 'sync-create', 2, 'Synchronize the values of the source and target properties when creating the binding; the direction of the synchronization is always from the source to the target.');
 
 INSERT INTO property VALUES
 ('GBinding', 'flags', 'GBindingFlags', NULL, NULL, NULL, NULL, NULL, '2.26', NULL),
