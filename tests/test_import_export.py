a8e6a6968f1e2309c881d14ab24faaa71bb2ac4a
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-09-23 22:26:36 -0400

    Tests: use tarket tk as get_original_exported_as_str() param
    Remane gtk3/4 directories as gtk+-3.0 gtk-4.0

--- tests/test_import_export.py
+++ tests/test_import_export.py
@@ -2,21 +2,21 @@
 import .ui files into cambalache and export to compare results
 """
 import os
-from io import BytesIO
+
 from lxml import etree
 from cambalache import CmbProject
 
-def get_original_exported_as_str(gtk_version,filename):
+def get_original_exported_as_str(target_tk, filename):
     """
-    import .ui file and return the original and a new one exported, both like a string
+    import .ui file and return the original, exported as a string tuple
     """
-    path = os.path.join(os.path.dirname(__file__), f'gtk{gtk_version}', filename)
+    path = os.path.join(os.path.dirname(__file__), target_tk, filename)
     str_original = open(path, 'r').read()
 
-    project = CmbProject(target_tk=('gtk+-3.0' if gtk_version==3 else 'gtk-4.0'))
+    project = CmbProject(target_tk=target_tk)
     ui_id = project.db.import_file(path)
     tree_exported = project.db.export_ui(ui_id, use_id=False)
-    str_exported=etree.tostring(tree_exported,
+    str_exported = etree.tostring(tree_exported,
                       pretty_print=True,
                       xml_declaration=True,
                       encoding='UTF-8').decode('UTF-8')
@@ -27,12 +27,12 @@ def test_gtk3_gtkwindow_ui():
     """
     test for gtkwindow.ui file in gtk3 version
     """
-    original, exported = get_original_exported_as_str(3,"gtkwindow.ui")
+    original, exported = get_original_exported_as_str("gtk+-3.0", "gtkwindow.ui")
     assert  original == exported
 
 def test_gtk4_gtkwindow_ui():
     """
     test for gtkwindow.ui file in gtk4 version
     """
-    original, exported = get_original_exported_as_str(4,"gtkwindow.ui")
+    original, exported = get_original_exported_as_str("gtk-4.0", "gtkwindow.ui")
     assert  original == exported
