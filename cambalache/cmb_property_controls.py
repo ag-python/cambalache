a7f0e4e9a5a02da69277561d5f390503b3055b89
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2022-02-15 17:46:41 -0500

    CmbChildTypeComboBox: add new property control.
    Add widget to select child type

--- cambalache/cmb_property_controls.py
+++ cambalache/cmb_property_controls.py
@@ -372,7 +372,6 @@ class CmbToplevelChooser(Gtk.ComboBoxText):
     object = GObject.Property(type=CmbUI, flags = GObject.ParamFlags.READWRITE)
 
     def __init__(self, **kwargs):
-        self._value = None
         super().__init__(**kwargs)
         self.connect('notify::object', self.__on_object_notify)
         self.connect('changed', self.__on_changed)
@@ -417,3 +416,53 @@ class CmbToplevelChooser(Gtk.ComboBoxText):
 
         self.set_active_id(str(value) if value is not None else None)
 
+
+class CmbChildTypeComboBox(Gtk.ComboBox):
+    __gtype_name__ = 'CmbChildTypeComboBox'
+
+    object = GObject.Property(type=GObject.Object, flags = GObject.ParamFlags.READWRITE)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('changed', self.__on_changed)
+
+        # Model, store it in a Python variable to make sure we hold a reference
+        # First column is the ID and the second is if you can select the child or not
+        self.__model = Gtk.ListStore(str, bool)
+        self.props.model = self.__model
+        self.props.id_column = 0
+
+        # Simple cell renderer
+        renderer_text = Gtk.CellRendererText()
+        self.pack_start(renderer_text, True)
+        self.add_attribute(renderer_text, "text", 0)
+        self.add_attribute(renderer_text, "sensitive", 1)
+
+        self.__populate_model()
+
+    def __populate_model(self):
+        self.__model.clear()
+
+        parent = self.object.parent
+        if parent is None:
+            return
+
+        self.__model.append([None, True])
+
+        pinfo = parent.info
+        while pinfo:
+            if pinfo.child_types:
+                for t in pinfo.child_types:
+                    self.__model.append([t, True])
+            pinfo = pinfo.parent
+
+    def __on_changed(self, obj):
+        self.notify('cmb-value')
+
+    @GObject.Property(type=str)
+    def cmb_value(self):
+        return self.props.active_id
+
+    @cmb_value.setter
+    def _set_cmb_value(self, value):
+        self.props.active_id = value
