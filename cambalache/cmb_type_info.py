6fcaccf20a7ebf61e8a55c1b5570a490310b2db5
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2022-03-10 17:36:43 -0300

    CmbTypeInfo: initialize public members
    Add logic from CmbProject in the type info class directly.

--- cambalache/cmb_type_info.py
+++ cambalache/cmb_type_info.py
@@ -1,7 +1,7 @@
 #
 # Cambalache Type Info wrapper
 #
-# Copyright (C) 2021  Juan Pablo Ugarte
+# Copyright (C) 2021-2022  Juan Pablo Ugarte
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -25,7 +25,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import GObject, Gtk
 
-from .cmb_objects_base import CmbBaseTypeInfo, CmbBaseTypeDataInfo, CmbBaseTypeDataArgInfo, CmbTypeChildInfo
+from .cmb_objects_base import CmbBaseTypeInfo, CmbBaseTypeDataInfo, CmbBaseTypeDataArgInfo, CmbTypeChildInfo, CmbPropertyInfo, CmbSignalInfo
 
 
 class CmbTypeDataArgInfo(CmbBaseTypeDataArgInfo):
@@ -44,11 +44,12 @@ class CmbTypeInfo(CmbBaseTypeInfo):
     parent = GObject.Property(type=GObject.Object, flags = GObject.ParamFlags.READWRITE)
 
     def __init__(self, **kwargs):
-        self.hierarchy = []
-        self.properties = {}
-        self.signals = {}
-        self.data = {}
         super().__init__(**kwargs)
+
+        self.hierarchy = self.__init_hierarchy()
+        self.properties = self.__init_properties_signals(CmbPropertyInfo, 'property')
+        self.signals = self.__init_properties_signals(CmbSignalInfo, 'signal')
+        self.data = self.__init_data()
 
         if self.parent_id == 'enum':
             self.enum = self.__init_enum_flags('enum')
@@ -57,11 +58,95 @@ class CmbTypeInfo(CmbBaseTypeInfo):
 
         self.child_types = self.__init_child_type()
 
+    def __init_hierarchy(self):
+        retval = []
+
+        c = self.project.db.cursor()
+        for row in c.execute('''
+                    WITH RECURSIVE ancestor(type_id, generation, parent_id) AS (
+                      SELECT type_id, 1, parent_id FROM type
+                        WHERE parent_id IS NOT NULL AND
+                              parent_id != 'interface' AND
+                              parent_id != 'enum' AND
+                              parent_id != 'flags' AND
+                              type_id=?
+                      UNION ALL
+                      SELECT ancestor.type_id, generation + 1, type.parent_id
+                        FROM type
+                        JOIN ancestor ON type.type_id = ancestor.parent_id
+                        WHERE type.parent_id IS NOT NULL AND
+                            type.parent_id != 'object' AND
+                            ancestor.type_id=?
+                    )
+                    SELECT parent_id, generation FROM ancestor
+                    UNION
+                    SELECT type_iface.iface_id, 0
+                      FROM ancestor JOIN type_iface
+                      WHERE ancestor.type_id = type_iface.type_id
+                    ORDER BY generation;''',
+                             (self.type_id, self.type_id)):
+            retval.append(row[0])
+
+        c.close()
+
+        return retval
+
+    def __init_properties_signals(self, Klass, table):
+        retval = {}
+
+        c = self.project.db.cursor()
+        for row in c.execute(f'SELECT * FROM {table} WHERE owner_id=? ORDER BY {table}_id;',
+                             (self.type_id, )):
+            retval[row[1]] = Klass.from_row(self, *row)
+
+        c.close()
+        return retval
+
+    def __type_get_data(self, owner_id, data_id, parent_id, key, type_id):
+        args = {}
+        children = {}
+        parent_id = parent_id if parent_id is not None else 0
+        retval = CmbTypeDataInfo.from_row(self, owner_id, data_id, parent_id, key, type_id)
+
+        c = self.project.db.cursor()
+
+        # Collect Arguments
+        for row in c.execute('SELECT * FROM type_data_arg WHERE owner_id=? AND data_id=?;',
+                             (owner_id, data_id)):
+            _key = row[2]
+            args[_key] = CmbTypeDataArgInfo.from_row(self, *row)
+
+        # Recurse children
+        for row in c.execute('SELECT * FROM type_data WHERE owner_id=? AND parent_id=?;',
+                             (owner_id, data_id)):
+            _key = row[3]
+            children[_key] = self.__type_get_data(*row)
+
+        c.close()
+
+        retval.args = args
+        retval.children = children
+
+        return retval
+
+    def __init_data(self):
+        retval = {}
+
+        c = self.project.db.cursor()
+        for row in c.execute('SELECT * FROM type_data WHERE parent_id IS NULL AND owner_id=? ORDER BY data_id;',
+                             (self.type_id, )):
+            key = row[3]
+            retval[key] = self.__type_get_data(*row)
+
+        c.close()
+        return retval
+
     def __init_child_type(self):
         retval = {}
 
-        for row in self.project.db.execute('SELECT * FROM type_child_type WHERE type_id=?',
-                                           (self.type_id, )):
+        c = self.project.db.cursor()
+        for row in c.execute('SELECT * FROM type_child_type WHERE type_id=?;',
+                             (self.type_id, )):
             type_id, child_type, max_children, linked_property_id = row
             retval[child_type] = CmbTypeChildInfo(project=self.project,
                                                   type_id=type_id,
@@ -69,14 +154,17 @@ class CmbTypeInfo(CmbBaseTypeInfo):
                                                   max_children=max_children if max_children else 0,
                                                   linked_property_id=linked_property_id)
 
-        return retval if len(retval.keys()) else None
+        c.close()
+        return retval
 
     def __init_enum_flags(self, name):
         retval = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING, GObject.TYPE_INT)
 
-        for row in self.project.db.execute(f'SELECT name, nick, value FROM type_{name} WHERE type_id=?', (self.type_id,)):
+        c = self.project.db.cursor()
+        for row in c.execute(f'SELECT name, nick, value FROM type_{name} WHERE type_id=?', (self.type_id,)):
             retval.append(row)
 
+        c.close()
         return retval
 
     def is_a(self, type_id):
